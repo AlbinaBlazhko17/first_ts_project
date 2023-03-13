2e397c87ef9d3d360603f5f71e09ba18f0bd1f96
Albinator <albinamivina@gmail.com>
2023-03-13 14:42:41 +0200

    add class to build ffmpeg command

--- dist/commands/ffmpeg/ffmpeg.buider.js
+++ dist/commands/ffmpeg/ffmpeg.buider.js
@@ -5,10 +5,21 @@ export class FfmpegBuilder {
     }
     input(inputPath) {
         this.inputPath = inputPath;
+        return this;
     }
     setVideoSize(width, height) {
+        this.options.set('-s', `${width}x${height}`);
+        return this;
     }
     output(outputPath) {
-        this.outputPath = outputPath;
+        if (!this.inputPath)
+            throw new Error('Doesn`t exist parametr input');
+        const args = ['-i', this.inputPath];
+        this.options.forEach((value, key) => {
+            args.push(key);
+            args.push(value);
+        });
+        args.push(outputPath);
+        return args;
     }
 }
--- src/commands/ffmpeg/ffmpeg.buider.ts
+++ src/commands/ffmpeg/ffmpeg.buider.ts
@@ -7,15 +7,24 @@ export class FfmpegBuilder {
         this.options.set('-c:v', 'libx265');
     }
 
-    input(inputPath: string) {
+    input(inputPath: string): this {
         this.inputPath = inputPath;
+        return this;
     }
 
-    setVideoSize(width: number, height: number){
-
+    setVideoSize(width: number, height: number): this{
+        this.options.set('-s', `${width}x${height}`);
+        return this;
     }
 
-    output(outputPath: string) {
-        this.outputPath = outputPath;
+    output(outputPath: string): string[] {
+        if(!this.inputPath) throw new Error('Doesn`t exist parametr input');
+        const args: string[] = ['-i', this.inputPath];
+        this.options.forEach((value, key) => {
+            args.push(key);
+            args.push(value);
+        });
+        args.push(outputPath);
+        return args;
     }
 }
